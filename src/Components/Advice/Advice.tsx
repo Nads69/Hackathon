d8ab3abd68d01a5a9fbf864bb891334ae2c67db2
nadir haddouche <nads@MacBook-Pro-de-Nadir.local>
2024-12-06 14:47:50 +0100

    final project |ready for presentation

--- src/Components/Advice/Advice.tsx
+++ src/Components/Advice/Advice.tsx
@@ -1,42 +1,34 @@
 import "./Advice.css";
-import AdviceObject from "../../utils/advice.json"
+import AdviceObject from "../../utils/advice.json";
 import { useEffect, useState } from "react";
 import { useCurrency } from "../../contexts/context";
 
-
-
-interface currencyData  {
+interface currencyData {
   country: string;
   gestes: string;
   phrase: string;
   conseil: string;
 }
 
-  export default function Advice() {
+export default function Advice() {
+  const [currencyData, setCurrencyData] = useState<currencyData | null>(null);
+  const [selectedCurrency, setSelectedCurrency] = useState("");
+  const { toList } = useCurrency();
+  const { isSubmitted } = useCurrency();
+  const [error, setError] = useState(null);
 
-    
-  const   [currencyData, setCurrencyData] = useState<currencyData | null>(null);
-  const   [selectedCurrency, setSelectedCurrency] = useState("");
-  const   {toList} = useCurrency();
-  const   {isSubmitted} = useCurrency();
-  const   [error, setError] = useState(null);
-
-
-  function findRightCurrencyInJsonList()
-  {
+  function findRightCurrencyInJsonList() {
     let index = 0;
 
-    while(Object.keys(AdviceObject.data)[index])
-    {
-      const   newObj = Object.entries(AdviceObject.data)[index]; 
+    while (Object.keys(AdviceObject.data)[index]) {
+      const newObj = Object.entries(AdviceObject.data)[index];
       //console.log(newObj[0])
       //console.log(index);
       //console.log(Object.values(AdviceObject.data)[index])
       //console.log(Object.entries(AdviceObject.data)[index])
 
-      if(newObj[0] == toList)
-      {
-        console.log(Object.values(AdviceObject.data)[index]);
+      if (newObj[0] == toList) {
+        //console.log(Object.values(AdviceObject.data)[index]);
         setCurrencyData(Object.values(AdviceObject.data)[index]);
       }
       index++;
@@ -45,28 +37,22 @@ interface currencyData  {
     return;
   }
 
-
   useEffect(() => {
-
-    const url = `http://localhost:3000/Currency?curr=${toList}`; 
+    const url = `http://localhost:3000/Currency?curr=${toList}`;
     setSelectedCurrency(toList);
     //console.log(url);
     //console.log(toList);
     //console.log(isSubmitted);
 
-    if(isSubmitted == false)
-    {
-      console.log("cannot launch fetch to get curr")
-    }
-    else
-    { 
-
+    if (isSubmitted == false) {
+      console.log("cannot launch fetch to get curr");
+    } else {
       fetch("http://localhost:3000/Currency?curr=USD", {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },}
-      )
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
         .then((res) => {
           if (!res.ok) {
             throw new Error("Erreur de récupération des données");
@@ -74,22 +60,19 @@ interface currencyData  {
           return res.json();
         })
         .then((data) => {
-          console.log( "this data from fetch ",data);
+          console.log("this data from fetch ", data);
           console.log("this data from Json in util", AdviceObject);
 
           findRightCurrencyInJsonList();
-
-       })
+        })
         .catch((error) => {
           console.error("Erreur:", error);
           setError(error.message);
         });
     }
-    
   }, [toList, isSubmitted]);
 
-
-/*
+  /*
   const handleCurrencyChange = (event) => {
     setSelectedCurrency(event.target.value);
   };*/
@@ -99,40 +82,36 @@ interface currencyData  {
   }
 
   if (!currencyData) {
-    return <div>Chargement...</div>;
+    return <div> </div>;
   }
 
   //const selectedCountryInfo = currencyData[selectedCurrency];
 
-
   return (
     <div className="advice-container">
-      <h1>Conseils de Voyage</h1>
+      <h1 className="titreconseil">Conseils de Voyage</h1>
 
-          <div className="country-details">
+      <div className="country-details">
+        <h2>{currencyData.country}</h2>
 
-              <h2>{currencyData.country}</h2>
+        <div className="detail-section">
+          <h3>Gestes culturels :</h3>
+          <p>{currencyData.gestes}</p>
+        </div>
 
-              <div className="detail-section">
-                <h3>Gestes culturels :</h3>
-                <p>{currencyData.gestes}</p>
-              </div>
+        <div className="detail-section">
+          <h3>Phrase utile :</h3>
+          <p>{currencyData.phrase}</p>
+        </div>
 
-              <div className="detail-section">
-                <h3>Phrase utile :</h3>
-                <p>{currencyData.phrase}</p>
-              </div>
-
-              <div className="detail-section">
-                <h3>Conseil de voyage :</h3>
-                <p>{currencyData.conseil}</p>
-              </div>
-
-          </div>
-
+        <div className="detail-section">
+          <h3>Conseil de voyage :</h3>
+          <p>{currencyData.conseil}</p>
+        </div>
+      </div>
     </div>
-  )
-/*
+  );
+  /*
   return (
     <div className="advice-container">
       <h1>Conseils de Voyage par Devise</h1>
--- src/components/ChooseCurrency.tsx
+++ src/components/ChooseCurrency.tsx
@@ -4,77 +4,59 @@ import { useCurrency } from "../contexts/context";
 import "../Components/ChooseCurrency.css";
 
 interface CurrencyData {
-    currencies: string[];
+  currencies: string[];
 }
 
-function computeBalance(result : number, resultInput :number )
-{
-    var product =  result * resultInput;
-    return Math.round(product * 100) / 100;
+function computeBalance(result: number, resultInput: number) {
+  var product = result * resultInput;
+  return Math.round(product * 100) / 100;
 }
 
-function ChooseCurrency()
-{
+function ChooseCurrency() {
+  const CurrencyRefs: CurrencyData = CurrencyReferences;
+  const [fromList, setFromList] = useState("");
+  //const   [toList, setToList] = useState("");
+  const { toList, setToList } = useCurrency();
+  const [balance, setBalance] = useState("");
+  //const   [isSubmitted, setIsSubmitted] = useState(false);
+  const [resultInput, setResultInput] = useState(0);
+  const { isSubmitted, setIsSubmitted } = useCurrency();
+  const [conversionResult, setConversionResult] = useState("");
 
-    const   CurrencyRefs : CurrencyData = CurrencyReferences;
-    const   [fromList, setFromList] = useState("");
-    //const   [toList, setToList] = useState("");
-    const   {toList, setToList} = useCurrency();
-    const   [balance, setBalance] = useState("");
-    //const   [isSubmitted, setIsSubmitted] = useState(false);
-    const   [resultInput, setResultInput] = useState(0);
-    const   {isSubmitted, setIsSubmitted} = useCurrency();
-    const   [conversionResult, setConversionResult] = useState("");
+  const apiKey = import.meta.env.VITE_CLIENT_API_KEY;
 
-    const   apiKey = import.meta.env.VITE_CLIENT_API_KEY;
-    
+  const HandleChoiceFrom = (name: string) => {
+    setFromList(name);
+  };
 
-    const HandleChoiceFrom = (name: string) => {
-        setFromList(name);
-        //console.log(name);
-    };
+  const HandleChoiceTo = (name: string) => {
+    setToList(name);
+  };
 
-    const HandleChoiceTo = (name: string) => {
-        setToList(name);
-        //console.log(name);
-    };
+  const handleBalanceInput = (input: string) => {
+    const isValid = [...input].every(
+      (char) => char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57
+    );
 
-    const handleBalanceInput = (input: string) => {
-
-        const isValid = [...input].every(
-            (char) => char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57
-          );
-      
-          if(isValid) {
-            setBalance(input);
-        }
-        const   resultInput = parseFloat(parseFloat(balance).toFixed(2)); 
-        setResultInput(resultInput);
-        //console.log( " ResultInput",resultInput);        
+    if (isValid) {
+      const resultInput = parseFloat(parseFloat(input).toFixed(2));
+      setBalance(input);
+      setResultInput(resultInput);
     }
+  };
 
-    const handleProcessInput = () => {
-        if(isSubmitted === false)
-        setIsSubmitted(true);
-        else if(isSubmitted === true)
-        setIsSubmitted(false);
-    }
-
+  const handleProcessInput = () => {
+    if (isSubmitted === false) setIsSubmitted(true);
+    else if (isSubmitted === true) setIsSubmitted(false);
+  };
 
   useEffect(() => {
-    
     const url = `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}&currencies=${toList}&base_currency=${fromList}`;
 
-    //console.log(url);
-
-    if(fromList === "" || toList === "" )
-    {
-        console.log("ChooseCurrency.tsx");
-    }
-    else{
-        //console.log("test");
-
-        fetch(url)
+    if (fromList === "" || toList === "") {
+      console.log("ChooseCurrency.tsx");
+    } else {
+      fetch(url)
         .then((response) => {
           if (!response.ok) {
             throw new Error("Error fetching data from API");
@@ -82,77 +64,74 @@ function ChooseCurrency()
           return response.json();
         })
         .then((responseJson) => {
-            console.log(responseJson);
+          console.log(responseJson);
 
-            const result = Object.values(responseJson.data)[0];
-            //console.log( " ComputeBlance",computeBalance(result, resultInput));
-            setConversionResult(computeBalance(result, resultInput));
-            //console.log(" result API",result);
-           
+          const result = Object.values(responseJson.data)[0];
+          setConversionResult(computeBalance(result, resultInput));
         })
         .catch((error) => {
           console.error(error);
         });
-
     }
-}, [fromList, toList, isSubmitted]);
+  }, [fromList, toList, isSubmitted]);
 
-	return (
-		<div>
-			<div className="currency-selectors">
-				<select
-					name="From"
-					id="FromCurrency"
-					className="currency-container"
-					onChange={(selection) => HandleChoiceFrom(selection.target.value)}
-				>
-					<option value="">From </option>
-					{CurrencyRefs.currencies.map((currency: string, index: number) => (
-						<option key={index} value={currency}>
-							{currency}
-						</option>
-					))}
-				</select>
+  return (
+    <div>
+      <div className="currency-selectors">
+        <select
+          name="From"
+          id="FromCurrency"
+          className="currency-container"
+          onChange={(selection) => HandleChoiceFrom(selection.target.value)}
+        >
+          <option value="">From </option>
+          {CurrencyRefs.currencies.map((currency: string, index: number) => (
+            <option key={index} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
 
-				<select
-					name="To"
-					id="ToCurrency"
-					className="currency-container"
-					onChange={(selection) => HandleChoiceTo(selection.target.value)}
-				>
-					<option value="">To</option>
-					{CurrencyRefs.currencies.map((currency: string, index: number) => (
-						<option key={index} value={currency}>
-							{currency}
-						</option>
-					))}
-				</select>
-			</div>
+        <select
+          name="To"
+          id="ToCurrency"
+          className="currency-container"
+          onChange={(selection) => HandleChoiceTo(selection.target.value)}
+        >
+          <option value="">To</option>
+          {CurrencyRefs.currencies.map((currency: string, index: number) => (
+            <option key={index} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
 
-
-			<div>
-				<input
-					type="text"
-					className="valeur-result"
-					value={balance}
-					onChange={(event) => handleBalanceInput(event.target.value)}
-					placeholder="Enter amount"
-				/>
-			</div>
-			<button onClick={handleProcessInput}>Submit</button>
-
-			<label>
-				<input
-					type="text"
-					className="valeur-result"
-					value={conversionResult}
-					placeholder="Conversion result"
-					readOnly
-				/>
-			</label>
-		</div>
-	);
-
+      <div>
+        <input
+          type="text"
+          className="valeur-result"
+          value={balance}
+          onChange={(event) => handleBalanceInput(event.target.value)}
+          placeholder="Enter amount"
+        />
+      </div>
+      <div className="buttonapp">
+        <button className="buttonappChildren" onClick={handleProcessInput}>
+          Submit
+        </button>
+      </div>
+      <label>
+        <input
+          type="text"
+          className="valeur-result"
+          value={conversionResult}
+          placeholder="Conversion result"
+          readOnly
+        />
+      </label>
+    </div>
+  );
 }
 
 export default ChooseCurrency;
