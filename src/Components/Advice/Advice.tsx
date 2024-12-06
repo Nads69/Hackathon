dc989f30fd8756157018931a2cabfe0fd1e38670
User-Alnz <alexandre.anavoizard@gmail.com>
2024-12-06 13:24:18 +0100

    Advice.tsx Component logic done | Waiting for futher correction + formating CSS

--- src/Components/Advice/Advice.tsx
+++ src/Components/Advice/Advice.tsx
@@ -1,54 +1,98 @@
 import "./Advice.css";
+import AdviceObject from "../../utils/advice.json"
 import { useEffect, useState } from "react";
 import { useCurrency } from "../../contexts/context";
 
 
+
+interface currencyData  {
+  country: string;
+  gestes: string;
+  phrase: string;
+  conseil: string;
+}
+
   export default function Advice() {
 
-
-  const   [currencyData, setCurrencyData] = useState(null);
-  const   [selectedCurrency, setSelectedCurrency] = useState(null);
+    
+  const   [currencyData, setCurrencyData] = useState<currencyData | null>(null);
+  const   [selectedCurrency, setSelectedCurrency] = useState("");
   const   {toList} = useCurrency();
   const   {isSubmitted} = useCurrency();
   const   [error, setError] = useState(null);
 
 
+  function findRightCurrencyInJsonList()
+  {
+    let index = 0;
+
+    while(Object.keys(AdviceObject.data)[index])
+    {
+      const   newObj = Object.entries(AdviceObject.data)[index]; 
+      //console.log(newObj[0])
+      //console.log(index);
+      //console.log(Object.values(AdviceObject.data)[index])
+      //console.log(Object.entries(AdviceObject.data)[index])
+
+      if(newObj[0] == toList)
+      {
+        console.log(Object.values(AdviceObject.data)[index]);
+        setCurrencyData(Object.values(AdviceObject.data)[index]);
+      }
+      index++;
+    }
+
+    return;
+  }
+
+
   useEffect(() => {
 
     const url = `http://localhost:3000/Currency?curr=${toList}`; 
-    console.log(url);
+    setSelectedCurrency(toList);
+    //console.log(url);
     //console.log(toList);
     //console.log(isSubmitted);
 
-    fetch("http://localhost:3000/Currency?curr=USD", {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },}
-    )
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Erreur de récupération des données");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setCurrencyData(data);
-        // Sélectionner une devise par défaut (par exemple, la première)
-        const firstCurrency = Object.keys(data)[0];
-        setSelectedCurrency(firstCurrency);
-      })
-      .catch((error) => {
-        console.error("Erreur:", error);
-        setError(error.message);
-      });
+    if(isSubmitted == false)
+    {
+      console.log("cannot launch fetch to get curr")
+    }
+    else
+    { 
+
+      fetch("http://localhost:3000/Currency?curr=USD", {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },}
+      )
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Erreur de récupération des données");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log( "this data from fetch ",data);
+          console.log("this data from Json in util", AdviceObject);
+
+          findRightCurrencyInJsonList();
+
+       })
+        .catch((error) => {
+          console.error("Erreur:", error);
+          setError(error.message);
+        });
+    }
+    
   }, [toList, isSubmitted]);
 
 
-
+/*
   const handleCurrencyChange = (event) => {
     setSelectedCurrency(event.target.value);
-  };
+  };*/
 
   if (error) {
     return <div>Erreur de chargement : {error}</div>;
@@ -58,10 +102,37 @@ import { useCurrency } from "../../contexts/context";
     return <div>Chargement...</div>;
   }
 
-  const selectedCountryInfo = currencyData[selectedCurrency];
+  //const selectedCountryInfo = currencyData[selectedCurrency];
 
 
+  return (
+    <div className="advice-container">
+      <h1>Conseils de Voyage</h1>
 
+          <div className="country-details">
+
+              <h2>{currencyData.country}</h2>
+
+              <div className="detail-section">
+                <h3>Gestes culturels :</h3>
+                <p>{currencyData.gestes}</p>
+              </div>
+
+              <div className="detail-section">
+                <h3>Phrase utile :</h3>
+                <p>{currencyData.phrase}</p>
+              </div>
+
+              <div className="detail-section">
+                <h3>Conseil de voyage :</h3>
+                <p>{currencyData.conseil}</p>
+              </div>
+
+          </div>
+
+    </div>
+  )
+/*
   return (
     <div className="advice-container">
       <h1>Conseils de Voyage par Devise</h1>
@@ -102,4 +173,5 @@ import { useCurrency } from "../../contexts/context";
       )}
     </div>
   );
+  */
 }
